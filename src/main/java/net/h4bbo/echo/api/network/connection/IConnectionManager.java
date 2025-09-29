888bfa594bd5c13451535a7a1336888d723a1584
Quackster <quacksterr.15@gmail.com>
2025-09-29 20:45:16 +1000

    use connection manager

--- src/main/java/net/h4bbo/echo/api/network/connection/IConnectionManager.java
+++ src/main/java/net/h4bbo/echo/api/network/connection/IConnectionManager.java
@@ -2,34 +2,18 @@ package net.h4bbo.echo.api.network.connection;
 
 import io.netty.channel.Channel;
 
+import java.util.List;
+
 /**
  * Manages network connections and their associated sessions.
  * Provides methods to add, remove, and retrieve connection sessions.
  */
 public interface IConnectionManager extends IConnectionSend {
+    void addConnection(IConnectionSession session);
 
-    /**
-     * Adds a new connection for the given channel.
-     *
-     * @param channel the network channel to add
-     * @throws IllegalArgumentException if channel is null
-     */
-    void addConnection(Channel channel);
+    void removeConnection(IConnectionSession session);
 
-    /**
-     * Removes an existing connection for the given channel.
-     *
-     * @param channel the network channel to remove
-     * @throws IllegalArgumentException if channel is null
-     */
-    void removeConnection(Channel channel);
-
-    /**
-     * Retrieves the connection session for the given channel.
-     *
-     * @param channel the network channel to look up
-     * @return the ConnectionSession associated with the channel, or null if not found
-     * @throws IllegalArgumentException if channel is null
-     */
     IConnectionSession getConnection(Channel channel);
+
+    List<IConnectionSession> getConnections();
 }
